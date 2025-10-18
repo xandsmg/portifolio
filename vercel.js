be844498c81970a742b4b2fb5d48f8ef924c72b8
xandsmg <joaoalexandresmg3@gmail.com>
2025-10-18 17:26:08 -0300

    test

--- vercel.js
+++ vercel.js
@@ -5,23 +5,5 @@
       "src": "**",
       "use": "@vercel/static"
     }
-  ],
-  "routes": [
-    {
-      "src": "/assets/(.*)",
-      "dest": "/public/assets/$1"
-    },
-    {
-      "src": "/css/(.*)",
-      "dest": "/public/css/$1"
-    },
-    {
-      "src": "/js/(.*)", 
-      "dest": "/public/js/$1"
-    },
-    {
-      "src": "/(.*)",
-      "dest": "/index.html"
-    }
   ]
 }
