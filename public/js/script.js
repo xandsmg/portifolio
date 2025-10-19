9e7a99d22db4393d14e19ed24de79d96b1c97369
xandsmg <joaoalexandresmg3@gmail.com>
2025-10-19 17:29:58 -0300

    test

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
+// COPYRIGHT
 document.getElementById("copyright-result").textContent = new Date().getFullYear()
-window.onload = function () {
-    const audio = document.getElementById("backgroundSound");
-    audio.volume = 0.3; 
-}
+
+
