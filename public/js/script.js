61586375c5af878762459f5a531b698f458428a3
xandsmg <joaoalexandresmg3@gmail.com>
2025-10-19 16:32:27 -0300

    test

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,5 @@
 document.getElementById("copyright-result").textContent = new Date().getFullYear()
+window.onload = function () {
+    const audio = document.getElementById("backgroundSound");
+    audio.volume = 0.3; 
+}
